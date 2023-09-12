0583505595b5e08f0f38f9f5c159951b054af746
Andrei <andreikravtsov88@gmail.com>
2023-09-12 11:09:08 +0300

    search style

--- myShop/src/app/components/search/Search.tsx
+++ myShop/src/app/components/search/Search.tsx
@@ -4,7 +4,10 @@ import style from './Search.module.scss'
 const Search = observer(() => {
     return (
         <div className={style.block__input}>
-            <input className={style.input} placeholder='Search property'/>
+            <input className={style.input} placeholder='Search property'>
+            </input>
+            <button className={style.btn}>Find Now</button>
+            <button className={style.btn__filter}>Filter</button>
         </div>
     )
 })
